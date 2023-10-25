3a2a855537f8ac1b1898e96a312ab69dca72c29b
FouadTaissate <fouad56@live.nl>
2023-10-25 18:47:38 +0200

    deel van footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,16 @@
 <footer>
-    <h1>Footer</h1>
-   </footer>
+
+    <img src="/public/images/logo.png" class="logo-footer" />
+    <section>
+        <h2>Snelle Links</h2>
+        <ul>
+            <li>Portfolio weergeven</li>
+            <li>Contact ons</li>
+            <li>Registreren</li>
+            <li>Log in</li>
+        </ul>
+    </section>
+    <section class="icons">
+
+    </section>
+</footer>
