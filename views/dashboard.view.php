1f172733290c222b84ea19c17dfb698a91761a70
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-10-29 16:18:27 +0100

    Build the front-end of the dashboard

---
+++ views/dashboard.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "partials/head.php" ?>
+
+<body>
+   <?php include "partials/header.php" ?>
+   <main class="dashboard-page">
+    <div class="cta-add-portfolio">
+        <a href="#" class="addPortfolioBtn">Voeg toe+</a>
+    </div>
+        <div class="dashboard-portfolio">
+            <div class="dashboard-portfolio-view-name">
+                <h4>Geen portfolio gevonden</h4>
+            </div>
+            <div class="dashboard-portfolio-crud">
+                <i>Edit</i>
+                <i>Delete</i>
+            </div>
+        </div>
+   </main>
+   <?php include "partials/footer.php" ?>
+</body>
+</html>
