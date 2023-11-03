e8037cfc9cbbaabbf8c2fb499f094586993d48d4
FouadTaissate <fouad56@live.nl>
2023-11-03 20:35:17 +0100

    izanann

--- views/partials/header.php
+++ views/partials/header.php
@@ -3,21 +3,34 @@
         <a href="/"><img src="/public/images/logo.png" alt="logo" class="logo-img"></a>
     </div>
     <nav>
-        <div class="menulist">
-            <ul>
+        <!-- <section class="menu-container">
+            <div class="menulist">
+                <ul class="menu">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/portfolio">Projecten</a></li>
+                    <li><a href="/contact">Contact ons</a></li>
+                </ul>
+            </div>
+        </section> -->
+        <section class="top-nav">
+            <input id="menu-toggle" type="checkbox" />
+            <label class='menu-button-container' for="menu-toggle">
+                <div class='menu-button'></div>
+            </label>
+            <ul class="menu">
                 <li><a href="/">Home</a></li>
                 <li><a href="/portfolio">Projecten</a></li>
                 <li><a href="/contact">Contact ons</a></li>
             </ul>
-        </div>
+        </section>
         <div class="cta-login-btn">
             <?php
             session_start();
 
             //Checks if user is logged in
             if (isset($_SESSION["email"])) {
-               echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
-            }else {
+                echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
+            } else {
                 echo '<a href="/login" class="cta-login">Log in</a>';
             }
             ?>
