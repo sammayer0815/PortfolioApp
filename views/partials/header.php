69956a52bf86ef99aa885bddb29a5833ab72e55d
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-11-04 00:05:46 +0100

    Added a overlay menu

--- views/partials/header.php
+++ views/partials/header.php
@@ -2,31 +2,33 @@
     <div class="logo">
         <a href="/"><img src="/public/images/logo.png" alt="logo" class="logo-img"></a>
     </div>
-    <nav>
-        <div class="cta-login-btn">
-            <?php
-            session_start();
-
-            //Checks if user is logged in
-            if (isset($_SESSION["email"])) {
-                echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
-            } else {
-                echo '<a href="/login" class="cta-login">Log in</a>';
-            }
-            ?>
-        </div>
-        <!-- <section class="menu-container">
+          <section class="menu-container">
             <div class="menulist">
-                <ul class="menu">
-                    <li><a href="/">Home</a></li>
+                <input type="checkbox" id="op">-</input>
+                <div class="lower">
+                <label for="op"><img src="/public/images/hamburger-menu.svg" class="hamburger-menu"/></label>
+                </div>
+                <div class="overlay overlay-hugeinc">
+			    <label for="op"></label>
+			<nav>
+				<ul>
+					<li><a href="/">Home</a></li>
                     <li><a href="/portfolio">Projecten</a></li>
                     <li><a href="/contact">Contact ons</a></li>
-                </ul>
+                    <li><?php
+                    session_start();
+                        //Checks if user is logged in
+                        if (isset($_SESSION["email"])) {
+                            echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
+                        } else {
+                            echo '<a href="/login" class="cta-login">Log in</a>';
+                        }
+                            ?></li>
+
+				</ul>
+			</nav>
             </div>
-        </section> -->
-
-
-
-    </nav>
-
+            </div>
+        </section>
+</div>
 </header>
