a082dbf3251f528e2583bc226a2556c79bd168c3
FouadTaissate <fouad56@live.nl>
2023-11-03 22:12:19 +0100

    login button inside dropdown

--- views/partials/header.php
+++ views/partials/header.php
@@ -13,7 +13,7 @@
             </div>
         </section> -->
 
-        
+
         <section class="top-nav">
             <input id="menu-toggle" type="checkbox" />
             <label class='menu-button-container' for="menu-toggle">
@@ -23,18 +23,20 @@
                 <li><a href="/">Home</a></li>
                 <li><a href="/portfolio">Projecten</a></li>
                 <li><a href="/contact">Contact ons</a></li>
-                <div class="cta-login-btn">
-                <?php
-                session_start();
+                <li>
+                    <div class="cta-login-btn">
+                        <?php
+                        session_start();
 
-                //Checks if user is logged in
-                if (isset($_SESSION["email"])) {
-                    echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
-                } else {
-                    echo '<a href="/login" class="cta-login">Log in</a>';
-                }
-                ?>
-                </div>
+                        //Checks if user is logged in
+                        if (isset($_SESSION["email"])) {
+                            echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
+                        } else {
+                            echo '<a href="/login" class="cta-login">Log in</a>';
+                        }
+                        ?>
+                    </div>
+                </li>
             </ul>
         </section>
     </nav>
