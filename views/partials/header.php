f8e559b45e5a323e8a190312b17f42802e46d97b
FouadTaissate <fouad56@live.nl>
2023-11-03 22:51:06 +0100

    s

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,6 +1,20 @@
 <header>
-
+    <div class="logo">
+        <a href="/"><img src="/public/images/logo.png" alt="logo" class="logo-img"></a>
+    </div>
     <nav>
+        <div class="cta-login-btn">
+            <?php
+            session_start();
+
+            //Checks if user is logged in
+            if (isset($_SESSION["email"])) {
+                echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
+            } else {
+                echo '<a href="/login" class="cta-login">Log in</a>';
+            }
+            ?>
+        </div>
         <!-- <section class="menu-container">
             <div class="menulist">
                 <ul class="menu">
@@ -12,33 +26,7 @@
         </section> -->
 
 
-        <section class="top-nav">
-            <input id="menu-toggle" type="checkbox" />
-            <label class='menu-button-container' for="menu-toggle">
-                <div class='menu-button'></div>
-            </label>
-            <ul class="menu">
-                <li><a href="/">Home</a></li>
-                <li><a href="/portfolio">Projecten</a></li>
-                <li><a href="/contact">Contact ons</a></li>
-                <li>
-                    <div class="cta-login-btn">
-                        <?php
-                        session_start();
 
-                        //Checks if user is logged in
-                        if (isset($_SESSION["email"])) {
-                            echo '<a href="/dashboard" class="cta-login">Dashboard</a>';
-                        } else {
-                            echo '<a href="/login" class="cta-login">Log in</a>';
-                        }
-                        ?>
-                    </div>
-                </li>
-            </ul>
-        </section>
     </nav>
-    <div class="logo">
-        <a href="/"><img src="/public/images/logo.png" alt="logo" class="logo-img"></a>
-    </div>
+
 </header>
