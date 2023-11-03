236ff85c29aa6992d65eea40f954545cb18e4e7f
FouadTaissate <fouad56@live.nl>
2023-11-03 14:05:57 +0100

    izan2

--- views/portfolio-person.view.php
+++ views/portfolio-person.view.php
@@ -6,34 +6,47 @@
 <body>
     <?php include "partials/header.php" ?>
     <div class="container-personview">
-    <section class="place-holder">
-        <img src="/public/images/Nilo.png" />
-        <h2 class="Name">naam</h2>
-        <p>Email: <br>Tel:</p>
-    </section>
-    <section class="place-holder2">
-        <h2>Wie ben ik?</h2>
-        <p>
-            placeholder verhaal f f dsf fds fds fds dfs dfs f fs <br>fds fs fsd fs dfs fsd dfsdfs dfs dfs dfs dfsdfs dfs<br> dfs dfs fsdfs dfs dfs dsf dfs dsf<br> fd fds sf sf sf sf
-        </p>
-    </section>
-    <h1>Skills</h1>
-    <section class="place-holder3">
-        <p>PLaceholder</p>
-        <p>placeholder</p>
-    </section>
-    <h1>Hobbies</h1>
-    <section class="place-holder4">
-        <p>Hobby</p><br>
-        <p>Hobby2</p>
-    </section>
-    <h1>Projecten</h1>
-    <section class="place-holder4">
-        <p>Project 1</p>
-        <p>Project 2</p>
-    </section>
+        <section class="place-holder">
+            <img src="/public/images/Nilo.png" />
+            <h2 class="Name">naam</h2>
+            <p>Email: <br>Tel:</p>
+        </section>
+        <section class="place-holder2">
+            <h2>Wie ben ik?</h2>
+            <p>
+                placeholder verhaal f f dsf fds fds fds dfs dfs f fs <br>fds fs fsd fs dfs fsd dfsdfs dfs dfs dfs dfsdfs dfs<br> dfs dfs fsdfs dfs dfs dsf dfs dsf<br> fd fds sf sf sf sf
+            </p>
+        </section>
+        <h1>Skills</h1>
+        <section class="place-holder3">
+            <p>PLaceholder</p>
+            <p>placeholder</p>
+        </section>
+        <h1>Hobbies</h1>
+        <section class="place-holder4">
+            <p>Hobby</p><br>
+            <p>Hobby2</p>
+        </section>
+        <h1>Projecten</h1>
+        <section class="place-holder4">
+            <div class="portofolio-card">
+                <img src="/public/images/Nilo.png" alt="foto persoon" class="profile-photo">
+                <h2>Niloyan Sellathurai</h2>
+                <a href="/profileview" class="cta-profileview">Bekijk</a>
+            </div>
+            <div class="portofolio-card">
+                <img src="/public/images/sam.png" alt="foto persoon" class="profile-photo">
+                <h2>Sam Maijer </h2>
+                <a href="/profileview" class="cta-profileview">Bekijk</a>
+            </div>
+            <div class="portofolio-card">
+                <img src="/public/images/Fouad.png" alt="foto persoon" class="profile-photo">
+                <h2>Fouad Taissate</h2>
+                <a href="/profileview" class="cta-profileview">Bekijk</a>
+            </div>
+        </section>
 
-</div>
+    </div>
 </body>
 <?php include "partials/footer.php" ?>
 
